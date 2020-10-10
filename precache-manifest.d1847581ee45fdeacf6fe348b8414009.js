9f02f5c48ce683fcee43f40058d0d56344517fe0
GyuryHan <pos7271@daum.net>
2020-10-10 13:54:42 +0900

    Updates

---
+++ precache-manifest.d1847581ee45fdeacf6fe348b8414009.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97697ff053280dd71901adc08b095d59",
+    "url": "/todo_list/index.html"
+  },
+  {
+    "revision": "0b75523d5b09ea885fc1",
+    "url": "/todo_list/static/css/main.9a13291e.chunk.css"
+  },
+  {
+    "revision": "06e99f2a1819642acf67",
+    "url": "/todo_list/static/js/2.ff554275.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo_list/static/js/2.ff554275.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b75523d5b09ea885fc1",
+    "url": "/todo_list/static/js/main.74a7588b.chunk.js"
+  },
+  {
+    "revision": "a48b75fa534a3d1c1ff9",
+    "url": "/todo_list/static/js/runtime-main.947fd4c1.js"
+  }
+]);
